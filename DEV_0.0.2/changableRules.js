4125349877997ee1a7e7a983b7c9a6d776e0a566
Patryk "patYczakus" Topolski <75316297+patYczakus@users.noreply.github.com>
2023-12-03 12:45:17 +0100

    Add files via upload

---
+++ DEV_0.0.2/changableRules.js
@@ -0,0 +1,20 @@
+var changableRules = [
+    {
+        description: "Czwórka jest wymuszonym dobraniem jednej karty.",
+        requiredRules: [],
+        blockingRules: [],
+        tags: [],
+    },
+    {
+        description: "Dziewiątka jest wymuszonym dobraniem jednej karty i czekania kolejkę.",
+        requiredRules: [],
+        blockingRules: [],
+        tags: [],
+    },
+    {
+        description: "Dziewiątka wino (♠️) wymusza na poprzedniego gracza.",
+        requiredRules: [2],
+        blockingRules: [],
+        tags: [],
+    },
+]
