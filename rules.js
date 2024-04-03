f0a07f8ce3d3d113dbba61035a58632f95146342
Patyczakus <75316297+Patyczakus@users.noreply.github.com>
2024-04-03 19:48:19 +0200

    Trochę ulepszenie zasad

--- rules.js
+++ rules.js
@@ -6,7 +6,7 @@ const rules = [
         description: `Jeżeli zostały graczowi dwie karty, zmuszony jest wypowiedzenie słowa "immunitet" po położeniu lub przed. Za nie powiedzenie tego gracz dobiera 4 karty.`,
         blockingRules: [],
         requiredRules: [],
-        addtionalInfo: 'W przypadku "Easy ROTeD" dobiera się 2 karty',
+        addtionalInfo: 'W przypadku "Easy ROTeD" dobiera się 2 karty\n[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]',
     },
     {
         description: `Można wyrzucać kilka kart tej samej liczby.`,
@@ -24,7 +24,7 @@ const rules = [
         description: `As powoduje zmianę koloru (czerwo/dzwonek/wino/żołędź).`,
         blockingRules: [2, 12],
         requiredRules: [],
-        addtionalInfo: null,
+        addtionalInfo: "[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]",
     },
     {
         description: `Joker powoduje położenie jednocześnie maksymalnie 3 kart zależnych od koloru Jokera - czerwony Joker powoduje możliwość położenia dowolnej karty w kolorze czerwa i dzwonka, a czarny - wina i żołędzia. Kładzenie jednocześnie kart oznacza, że te karty nie mają możliwości wykonania funkcji.`,
@@ -36,13 +36,13 @@ const rules = [
         description: `Czarny Walet (wino (♠️) i żołędź (♣️)) są żądaniami kart; można kłaść każdą kartę od 2 do 9, ale nie mogą wykonywać funkcji.`,
         blockingRules: [],
         requiredRules: [],
-        addtionalInfo: null,
+        addtionalInfo: "[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]",
     },
     {
         description: `Można przebijać żądania czarnego Waleta (wino/żołędź) każdym Waletem (czerwo/dzwonek/wino/żołędź)`,
         blockingRules: [],
         requiredRules: [5],
-        addtionalInfo: null,
+        addtionalInfo: "[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]",
     },
     {
         description: `Czwórka jest <u>wymuszonym dobraniem jednej karty</u> dla następnego gracza i nie powoduje jego blokady. Jeżeli jest włączona zasada nr. 2, można maksymalnie położyć parę kart (czyli dwie) - powoduje to <u>wymuszone dobranie aż trzech kart</u>.`,
@@ -51,22 +51,22 @@ const rules = [
         addtionalInfo: null,
     },
     {
-        description: `Dwójka i trójka powodują dobranie kart według ich wartości. Można je przebijać równą lub wyższą co do wartości kartą (czyli do Trójki); jeżeli jest to wyższa, musi być odpowiednia do koloru.`,
+        description: `Dwójka i trójka powodują dobranie kart według ich wartości. Można je przebijać równą lub wyższą co do wartości kartą (czyli do trójki); jeżeli jest to wyższa, musi być odpowiednia do koloru.`,
         blockingRules: [],
         requiredRules: [],
-        addtionalInfo: null,
+        addtionalInfo: "[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]",
     },
     {
         description: `Przestaje funkcjonować w zasadzie nr. 9 potrzeba położenia wyższej karty do koloru.`,
-        blockingRules: [8],
-        requiredRules: [],
+        blockingRules: [],
+        requiredRules: [8],
         addtionalInfo: null,
     },
     {
         description: `Przez całą rundę nie można zadawać pytań; za każde pytanie gracz dobiera jedną kartę.`,
         blockingRules: [],
         requiredRules: [],
-        addtionalInfo: null,
+        addtionalInfo: "[ Ta zasada działa na podobnej zasadzie co w grze BARTOK ]",
     },
     {
         description: `Wyrzucenie najpierw Czwórek, a później Jokera tworzy <i>combo</i> - daje reszcie graczy po karcie i pozwala na wyrzucenie tylu dowolnych kart, ile zostało wyrzuconych Czwórek; ostatnia wyrzucona karta może wykonać funkcję, jeżeli posiada.`,
@@ -87,6 +87,12 @@ const rules = [
         addtionalInfo: null,
     },
     {
+        description: `Piątka jest blokadą przeciwnika; możliwe przebijanie; im więcej kart, tym więcej czekania kolejek. Funkcje wtedy idą na kolejną osobę.`,
+        blockingRules: [],
+        requiredRules: [],
+        addtionalInfo: null,
+    },
+    {
         description: `Czarna dziewiątka jest <u>wymuszoną</u> blokadą przeciwnika i dobraniem jednej karty. Jest niezależna od zasady nr. 2 i niemożliwe jest wyłożenie kilku kart naraz.`,
         blockingRules: [],
         requiredRules: [],
@@ -99,10 +105,10 @@ const rules = [
         addtionalInfo: null,
     },
     {
-        description: `W dziewiątce tylko dobranie jest wymuszone, a blokady się kumulują. Jeżeli następna osoba (lub poprzednia z  nie będzie miała dziewiątki, czeka tyle kolejek, ile dziewiątek pod rząd.`,
+        description: `W dziewiątce tylko dobranie jest wymuszone, a blokady się kumulują. Jeżeli następna osoba (lub poprzednia z zasadą nr. 17) nie będzie miała dziewiątki, czeka tyle kolejek, ile dziewiątek pod rząd.`,
         blockingRules: [],
         requiredRules: [15],
-        addtionalInfo: null,
+        addtionalInfo: "Można wtedy przebijać każdą dziewiątką, ale to czarna dziewiątka musi rozpocząć kumulowanie blokad.",
     },
     {
         description: `Król to tarcza, która chroni przed brakiem wypowiedzenia “immunitet”. Jeżeli ktoś zauważy brak wypowiedzenia przez wytyczoną osobę, ten grać może przerwać wykonującemu w celu postawieniu króla, odpowiedniego do ostatniej karty. Osoba wytyczająca (ta, która zauważyła brak wypowiedzenia), <u>musi</u> wziąć z talii dwie karty.`,
@@ -120,6 +126,12 @@ const rules = [
         description: `Król powoduje dobranie czterech kart, dama tego samego koloru przebija dla wcześniejszego gracza.`,
         blockingRules: [18],
         requiredRules: [],
-        addtionalInfo: "W zasadzie nr. 9 jest wyższa od trójki",
+        addtionalInfo: "W zasadzie nr. 9 jest wyższa od trójki\n[ Ta zasada działa na podobnej zasadzie co w grze MAKAO ]",
+    },
+    {
+        description: `Dziewiątka i piątka mogą kumulować blokady wzajemnie ze sobą.`,
+        blockingRules: [],
+        requiredRules: [14, 17],
+        addtionalInfo: null,
     },
 ]
